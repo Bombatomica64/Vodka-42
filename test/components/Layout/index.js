c2a1e93756dfe95102721c75690b1fe39840129f
Bombatomica_64 <qualcosa2001@gmail.com>
2024-04-24 17:21:04 +0200

    first

---
+++ test/components/Layout/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Layout = () => {
+	return (
+		<div>
+			{
+				// Header
+			}
+			{
+				// Body
+			}
+			{
+				// Footer
+			}
+		</div>
+	);
+};
+
+export default Layout;
